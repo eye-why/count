d51093365271158ca6f799817d27c54f932ae723
eyewhy <79839924+eye-why@users.noreply.github.com>
2023-07-07 09:21:46 +0900

    [modify] count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Count = () => {
+
+    //use state
+    const [number, setNumber] = useState(0);
+
+
+    //use callback
+    //useCallback hook이 number를 업데이트 시키는 새로운 함수를 만드는데 사용
+    //리액트가 setNumber가 변하더라도 매번 렌더링되는걸 방지. 메모이제이션(캐쉬)된 함수 실행
+
+    const countNum = useCallback(() => {
+        setNumber(numCount => numCount + 1);
+    }, []); //처음 컴포넌트가 실행 시만?
+
+    console.log("countNum" + countNum);
+    /*
+        const countNum = setNumber(prevCount => prevCount + 1);
+        console.log("countNumTwo" + countNumTwo)*/
+
+    return (
+        <button onClick={countNum}>{number}번 클릭 했습니다!</button>
+    )
 }
 
-export default App;
+
+
+export default Count;
